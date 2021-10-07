552bf0d69aed48704bdd4684893cd951e79dbb48
lvsheng <lvsheng@sensorsdata.cn>
2021-10-07 21:48:32 +0800

    ReferenceConfigCache缓存

--- src/main/java/com/jd/lvsheng/dubbo/client/test/ImportService.java
+++ src/main/java/com/jd/lvsheng/dubbo/client/test/ImportService.java
@@ -4,32 +4,36 @@ import org.apache.dubbo.common.utils.NetUtils;
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.utils.ReferenceConfigCache;
 import org.dubbo.server.api.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("importService")
 public class ImportService {
-	
-	@Autowired
-	private RegistryConfig    registry;
-	@Autowired
-	private ApplicationConfig application;
-	
-	private ReferenceConfig<DemoService> newReferenceConfig(String group) {
-		ReferenceConfig<DemoService> reference = new ReferenceConfig<DemoService>();
-		reference.setApplication(application);
-		reference.setRegistry(registry);
-		reference.setInterface(DemoService.class);
-		reference.setGroup(group);
-		return reference;
-	}
-	
-	public void port() {
-		ReferenceConfig<DemoService> service = newReferenceConfig("IM-CELL-ACCESS-" + NetUtils.getLocalAddress().getHostAddress() + "-"
-																		+ "25000");
-		DemoService ds = service.get();
-		System.out.println(ds.sayHello("ls"));
-		
-	}
+
+    @Autowired
+    private RegistryConfig    registry;
+    @Autowired
+    private ApplicationConfig application;
+
+    private ReferenceConfig<DemoService> newReferenceConfig(String group) {
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<DemoService>();
+        reference.setApplication(application);
+        reference.setRegistry(registry);
+        reference.setInterface(DemoService.class);
+        reference.setGroup(group);
+        return reference;
+    }
+
+    public void port() {
+        ReferenceConfig<DemoService> reference =
+            newReferenceConfig("IM-CELL-ACCESS-" + NetUtils.getLocalAddress().getHostAddress() + "-"
+                + "25000");
+        ReferenceConfigCache cache = ReferenceConfigCache.getCache();
+		// cache.get方法中会缓存 Reference对象，并且调用ReferenceConfig.get方法启动ReferenceConfig
+        DemoService xxxService = cache.get(reference);
+        System.out.println(xxxService.sayHello("ls"));
+
+    }
 }
